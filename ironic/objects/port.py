0f7c3daca6db014ace5089b5f95276b40514791f
mnachiappan <mnachiappan@paypal.com>
2015-09-09 21:43:38 -0700

    Fix for Join error and added device uudi

--- ironic/objects/port.py
+++ ironic/objects/port.py
@@ -167,7 +167,9 @@ class Port(base.IronicObject):
         """
         values = self.obj_get_changes()
         #Reade config, if static_provider is true get static IP from neutron
-        #Maha
+        #
+        if not values['extra']:
+            raise exception.NotAcceptable()
         net_uuid = values['extra']['net_uuid']
         if not net_uuid:
             raise exception.NotAcceptable()
@@ -176,19 +178,19 @@ class Port(base.IronicObject):
         port_dict = self.prepare()
         port_dict['port']['mac_address'] = values['address']
         port_dict['port']['network_id'] = net_uuid
-        port_dict['port']['name'] = 'metal-id-'+values['node_id']
-        port_dict['port']['device_id'] = values['node_id']
+        port_dict['port']['name'] = 'metal-id-{}'.format(str(values['node_id']))
+        port_dict['port']['device_id'] = str(values['node_id'])
         port_new = network_provider.create_port(port_dict,context.auth_token)
-
-        port_dict = network_provider.get_port(port_new['port'].get('id'),context.auth_token)
-        fixed_ips = port_dict.get('fixed_ips')
-
+        #port_dict = network_provider.get_port(port_new['port'].get('id'),context.auth_token)
+        fixed_ips = port_new.get('fixed_ips')
         if fixed_ips:
             ip_address = fixed_ips[0].get('ip_address', None)
             values['extra']['ip'] = ip_address
+            values['uuid'] = port_new['port'].get('id')
 
         if ip_address:
             LOG.debug("IP Address =====>>>>  %s.", ip_address)
+
         db_port = self.dbapi.create_port(values)
         self._from_db_object(self, db_port)
 
